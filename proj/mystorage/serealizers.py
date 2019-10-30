14e954df151a18178bb218799c99224f71ad1140
soohyunkim <sss4920@likelion.org>
2019-10-30 21:16:38 +0900

    center subject

--- proj/mystorage/serealizers.py
+++
@@ -1,9 +0,0 @@
-from .models import Essay
-from rest_framework import serializers
-
-class EssaySerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = Essay
-        fields = '__all__'
-
